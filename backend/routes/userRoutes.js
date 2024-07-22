75f0e2c8052bd7b6ab010670cd150d1f8d3d03b5
Vincenzo Perretta <vincenzoperretta96@gmail.com>
2024-07-22 20:59:43 +0200

    afammoc

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,61 @@
+import express from "express"; // Importa il pacchetto Express
+import User from "../models/user.js"; // Importa il modello User
+
+const router = express.Router(); // Crea un router Express
+
+router.get("/profile", async (req, res) => {
+    try {
+      const users = await User.find({}); // Trova tutti gli utenti nel database
+      res.json(users); // Risponde con i dati degli utenti in formato JSON
+    } catch (err) {
+      res.status(500).json({ message: err.message }); // Gestisce errori e risponde con un messaggio di errore
+    }
+  });
+  
+  // Rotta per ottenere un singolo utente
+  router.get("/me", async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id); // Trova un utente per ID
+      if (!user) {
+        return res.status(404).json({ message: "Utente non trovato" }); // Se l'utente non esiste, risponde con un errore 404
+      }
+      res.json(user); // Risponde con i dati dell'utente in formato JSON
+    } catch (err) {
+      res.status(500).json({ message: err.message }); // Gestisce errori e risponde con un messaggio di errore
+    }
+  });
+  
+  // Rotta per creare un nuovo utente
+  router.post("/profile", async (req, res) => {
+    const user = new User(req.body); // Crea un nuovo utente con i dati dal corpo della richiesta
+    try {
+      const newUser = await user.save(); // Salva il nuovo utente nel database
+      res.status(201).json(newUser); // Risponde con i dati del nuovo utente e uno status 201 (Created)
+    } catch (err) {
+      res.status(400).json({ message: err.message }); // Gestisce errori di validazione e risponde con un messaggio di errore
+    }
+  });
+  
+  // Rotta per aggiornare un utente
+  router.put("/:id", async (req, res) => {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true, // Restituisce il documento aggiornato anziché quello vecchio
+      });
+      res.json(updatedUser); // Risponde con i dati dell'utente aggiornato in formato JSON
+    } catch (err) {
+      res.status(400).json({ message: err.message }); // Gestisce errori di validazione e risponde con un messaggio di errore
+    }
+  });
+  router.patch("/:id/image", async (req, res) => {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true, // Restituisce il documento aggiornato anziché quello vecchio
+      });
+      res.json(updatedUser); // Risponde con i dati dell'utente aggiornato in formato JSON
+    } catch (err) {
+      res.status(400).json({ message: err.message }); // Gestisce errori di validazione e risponde con un messaggio di errore
+    }
+  });
+
+  export default router; // Esporta il router per l'utilizzo in altri file
